638239ea6809757a8f1b6d8f7dfe1e5150c2a6c2
RikelmVoltz <142436269+RikelmVoltz@users.noreply.github.com>
2023-08-31 17:13:49 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,91 @@
+// rikelme voltz
+
+// EXERCICIO 1:
+
+//A: no console sera impresso o resultado 10 e 50
+
+//B:no console não sera imrpesso nada
+
+// EXERCICIO 2:
+
+//A: a função serve para deixar a frase do usuario toda em minuscula e ira perguntar se a palavra cenoura esta na frase.
+
+//B:será impresso no console true,true,false
+
+// escrita de código
+// ---------------------------1--------------------------------------------  
+/*
+A
+
+function informaçãoUsuario() {
+  console.log("Eu sou rikelme,tenho 16 anos moro em são leopoldo e sou estudante")
+}
+informaçãoUsuario();
+
+
+// B
+
+function imprimirinfo(nome,idade,endereço,profissao) {
+  return "eu sou " + nome, "tenho " + idade + " anos, " + "moro em " + endereço + " e sou " + profissao;
+}
+console.log(imprimirinfo('Rikelme', 16, 'SL', 'estudante'));
+
+
+
+// ---------------------------------------2---------------------------------
+
+//A 
+
+function somaNumeros(numero1,numero2){
+  return numero1 + numero2
+}
+console.log(somaNumeros(4,8));
+
+//B
+
+function maiorOuIgual(numero1,numero2){ 
+  return numero1 >= numero2;
+}
+console.log(numeromaior(6,8));
+
+//C
+
+function verificaImpar(numero1){
+    return (numero1 % 2 === 0);
+}
+console.log(verificaImpar(9));
+
+//D
+
+function tamanhoMaiuscula(msg){
+return msg.length + " " + msg.toUpperCase()
+}
+console.log(mensagem(prompt("lklkklkç"))); 
+
+
+
+
+
+
+
+// -------------------------3---------------------------------------------
+
+function soma(soma1,soma2){
+  return soma1 + soma2
+
+}
+function subtraçao(sub1,sub2){
+  return sub1 - sub2
+}
+function multiplicaçao(multi1,multi2){
+  return multi1 * multi2 
+}
+function divisao(divi1,divi2){
+  return divi1 / divi2
+}
+
+let num1 = Number(prompt("digite um numero para teste"));
+let num2 = Number(prompt("digite um segundo numero"));
+console.log(soma(num1,num2), subtraçao(num1,num2), multiplicaçao(num1,num2), divisao(num1,num2));
+
+*/
